e83e114c0e3a421a9090baebe6e648f3f3ed0bbf
ashit <ashitkoladiya7@gmail.com>
2022-07-13 08:18:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Doctors from './container/Doctors';
 import Form from './componets/Form';
 import Counter from './container/counter/Counter'
 import { ConfigStore } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
 
 function App() {
-  const store = ConfigStore();
+  const {store , persistor} = ConfigStore();
   return (
     <>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <Layout>
       <Switch>
         <Route exact path={"/Medicine"} component={Medicine}></Route>
@@ -24,6 +26,7 @@ function App() {
       </Switch>
 
     </Layout>
+    </PersistGate>
     </Provider>
     
     
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,18 @@
 import { createStore , applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { rootreducs } from './reducer';
 
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['counter']
+  }
+  const persistedReducer = persistReducer(persistConfig, rootreducs)
 export const ConfigStore = () => {
-    let store = createStore(rootreducs, applyMiddleware(thunk))
-    return store; 
+    let store = createStore(persistedReducer, applyMiddleware(thunk))
+    let persistor = persistStore(store)
+    return { store, persistor }
 }  
